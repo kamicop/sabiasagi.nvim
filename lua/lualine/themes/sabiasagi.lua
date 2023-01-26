8520cf72c797f3757880373e816537cf857e6f90
kamicop <t.kamico@gmail.com>
2023-01-26 22:18:04 +0900

    update

--- lua/lualine/themes/sabiasagi.lua
+++ lua/lualine/themes/sabiasagi.lua
@@ -1,6 +1,13 @@
 local M = {}
 local colors = require("sabiasagi.colors")
 
+-- M.normal = {
+-- 	a = { fg = colors.shade7, bg = colors.color6 },
+-- 	b = { fg = colors.shade6, bg = colors.background },
+-- 	c = { fg = colors.shade6, bg = colors.darker_background },
+-- 	x = { fg = colors.shade6, bg = colors.background },
+-- 	z = { fg = colors.shade7, bg = colors.color6 },
+-- }
 M.normal = {
 	a = { fg = colors.shade6, bg = colors.darker_background },
 	b = { fg = colors.shade6, bg = colors.shade0 },
@@ -9,9 +16,9 @@ M.normal = {
 	z = { fg = colors.shade6, bg = colors.darker_background },
 }
 
-M.insert = { a = { fg = colors.color6, bg = colors.shade0 } }
-M.visual = { a = { fg = colors.color2, bg = colors.shade0 } }
-M.replace = { a = { fg = colors.color6, bg = colors.shade0 } }
+M.insert = { a = { fg = colors.color6, bg = colors.darker_background } }
+M.visual = { a = { fg = colors.color3, bg = colors.darker_background } }
+M.replace = { a = { fg = colors.color15, bg = colors.darker_background } }
 
 M.inactive = {
 	a = { fg = colors.shade6, bg = colors.darker_background },
